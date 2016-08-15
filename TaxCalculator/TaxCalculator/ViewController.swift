62c37d5eee8018d8e9b9e0729e736d1c1648dd35
Julio Estrada <info@julioest.com>
2016-08-15 19:52:54 -0400

    Attempting to add Logic

--- TaxCalculator/TaxCalculator/ViewController.swift
+++ TaxCalculator/TaxCalculator/ViewController.swift
@@ -8,56 +8,79 @@
 
 import UIKit
 
-var stateTaxes = [
-    "CA", "CO", "NY", "TX"
-]
+let states = ["CA", "CO", "NY", "TX"]
+let taxPercents = [0.075, 0.0925, 0.045, 0.0825]
 
 class ViewController: UIViewController {
 
     let amountTextField: UITextField = UITextField()
-    var stateSegmentedControl = UISegmentedControl(items: stateTaxes)
-    var resultLabel: UILabel = UILabel()
+    let statesSegmentedControl = UISegmentedControl(items: states)
+    let totalLabel: UILabel = UILabel()
     let calcButton: UIButton = UIButton()
 
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+
         // Amount Text Field
         amountTextField.frame = CGRect(x: 20, y: 100, width: 240, height: 40)
         amountTextField.placeholder = "Enter Amount"
         amountTextField.font = UIFont.systemFontOfSize(15)
         amountTextField.borderStyle = UITextBorderStyle.RoundedRect
-//        amountTextField.keyboardType = UIKeyboardType.DecimalPad
+        amountTextField.keyboardType = UIKeyboardType.DecimalPad
         amountTextField.returnKeyType = UIReturnKeyType.Done
         amountTextField.contentVerticalAlignment = UIControlContentVerticalAlignment.Center
-        view.addSubview(amountTextField)
         
         // State Segmented Control
-        stateSegmentedControl.frame = CGRect(x: 20, y: 40, width: 320, height: 30)
-        stateSegmentedControl.selectedSegmentIndex = 0
-        stateSegmentedControl.tintColor = UIColor.blueColor()
-        stateSegmentedControl.addTarget(self, action: (#selector(segmentedValueChanged(_:))), forControlEvents: .AllEvents)
-        view.addSubview(stateSegmentedControl)
+        statesSegmentedControl.frame = CGRect(x: 20, y: 40, width: 320, height: 30)
+        statesSegmentedControl.selectedSegmentIndex = 0
+        statesSegmentedControl.tintColor = UIColor.blueColor()
+//        statesSegmentedControl.addTarget(self, action: (#selector(segmentedValueChanged(_:))), forControlEvents: .AllEvents)
+
 
         // Result Label
-        resultLabel.frame = CGRect(x: 20, y: 160, width: 200, height: 20)
-        resultLabel.textColor = UIColor.blackColor()
-        resultLabel.text = "Result Label"
-        view.addSubview(resultLabel)
+        totalLabel.frame = CGRect(x: 20, y: 220, width: 200, height: 20)
+        totalLabel.textColor = UIColor.blackColor()
+        totalLabel.text = " "
+        
         
         // Calculate Button
-        calcButton.frame = CGRect(x: 20, y: 140, width: 300, height: 40)
+        calcButton.frame = CGRect(x: 20, y: 160, width: 240, height: 40)
+        calcButton.backgroundColor = UIColor.blueColor()
+        calcButton.setTitle("Calculate", forState: .Normal)
+        calcButton.addTarget(self, action: #selector(buttonPressed(_:)), forControlEvents: .TouchUpInside)
+
+        view.addSubview(amountTextField)
+        view.addSubview(statesSegmentedControl)
+        view.addSubview(totalLabel)
+        view.addSubview(calcButton)
     }
 
-    func segmentedValueChanged(sender:UISegmentedControl!){
-        print("State of: \(sender.selectedSegmentIndex)" )
+//    func segmentedValueChanged(sender: UISegmentedControl!){
+//        print("State: \(sender.selectedSegmentIndex)" )
+//    }
+
+    func convertTaxPercent(percent: Double) -> Double {
+
+        var taxPercent: Double = 0.0
+
+        for tax in taxPercents {
+            taxPercent = taxPercent + tax
+        }
+
+        return taxPercent
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func buttonPressed(button: UIButton){
+//        let index = statesSegmentedControl.selectedSegmentIndex
+        var taxPercent: Double = 0.0
+
+        for tax in taxPercents {
+            taxPercent = (100 * tax)
+        }
+
+        print(taxPercent)
     }
 
 
