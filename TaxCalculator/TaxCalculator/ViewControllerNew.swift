ecf4978602d83d19c8fe16895b891a85d266ac6e
Gabe Morales <gmorales@phunware.com>
2016-08-16 14:41:25 -0500

    Nothing to see here

---
+++ TaxCalculator/TaxCalculator/ViewControllerNew.swift
@@ -0,0 +1,21 @@
+//
+//  ViewControllerNew.swift
+//  TaxCalculator
+//
+//  Created by Gabriel Morales on 8/16/16.
+//  Copyright © 2016 julioest. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerNew: UITableViewHeaderFooterView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
